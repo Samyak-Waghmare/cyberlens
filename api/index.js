82a6d098cdca8348cef23a3f8177ad2f67fba1bc
Samyak-Waghmare <samyakwaghmare347@gmail.com>
2026-06-22 17:51:50 +0530

    moved api to serverless function and simplified vercel config

---
+++ api/index.js
@@ -0,0 +1,5 @@
+// This file is the Vercel serverless function entry point for all /api/* routes.
+// It imports the Express app and exports it as a default handler.
+import app from "../server/index.js";
+
+export default app;
